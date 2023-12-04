6289dda508a6b8fdf91dde06431dbd982fbdfda2
JD Calvelli <jdcalvelli@10-18-132-64.dynapool.wireless.nyu.edu>
2023-12-04 18:03:23 -0500

    completing loop, no score yet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    [Header("Views")]
+    [SerializeField] private GameObject mainView;
+    [SerializeField] private GameObject gameOverView;
+
+    [Header("Controllers")]
     [SerializeField] private ScreenChange screenChangeController;
     [SerializeField] private LawraChange lawraChangeController;
     
@@ -21,9 +26,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        // input management
+        if (Input.GetKeyDown("space") 
+            && mainView.activeSelf)
         {
             screenChangeController.ChangeScreen();
+        }
+        else if (Input.GetKeyDown("r")
+                 && gameOverView.activeSelf)
+        {
+            Debug.Log("restart");
+            // need a more robust restart function
+            screenChangeController.ChangeScreen();
+            StartCoroutine(lawraChangeController.LawraBehavior());
+            //
+            mainView.SetActive(true);
+            gameOverView.SetActive(false);
+        }
+        
+        // check state
+        if (screenChangeController.currentState == ScreenChange.ComputerStates.GODOT 
+            && lawraChangeController.currentState == LawraChange.lawraStates.LOOKING)
+        {
+            Debug.Log("game over");
+            mainView.SetActive(false);
+            gameOverView.SetActive(true);
         }
     }
 }
